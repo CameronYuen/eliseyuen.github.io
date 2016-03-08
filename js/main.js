4d0a4785e0d73b8bd05a1e3ba78d7996e3f0899e
eliseyuen <skate4fun88@gmail.com>
2016-03-07 22:06:45 -0800

    added fullPage and progress on mobile view

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//
+// main.js
+// Prime3BFFs
+//
+// Created by Ian Cordero, Elise Yuen, Colin Fong, and Lena Correia on 11/3/15.
+// Copyright (c) 2015 UCLA Daily Bruin Online. All rights reserved.
+//
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	anchors:['firstPage', 'secondPage', 'thirdPage'],
+    	scrollOverflow: true,
+    	continuousVertical: false,
+    	slidesNavigation: true
+    });
+});
