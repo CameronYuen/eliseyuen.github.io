b5b22dde97beec3bca937f79fe063a8b3437fe7f
eliseyuen <skate4fun88@gmail.com>
2016-03-07 22:10:52 -0800

    js updates

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-//
-// main.js
-// Prime3BFFs
-//
-// Created by Ian Cordero, Elise Yuen, Colin Fong, and Lena Correia on 11/3/15.
-// Copyright (c) 2015 UCLA Daily Bruin Online. All rights reserved.
-//
-
 $(document).ready(function() {
     $('#fullpage').fullpage({
-    	anchors:['firstPage', 'secondPage', 'thirdPage'],
+    	anchors:['firstPage', 'secondPage'],
     	scrollOverflow: true,
     	continuousVertical: false,
     	slidesNavigation: true
     });
 });
+
+ $(window).load(function() {  document.getElementById("hideAll").style.display = "none"; });
